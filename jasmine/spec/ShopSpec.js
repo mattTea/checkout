0d17bd0342b4a1845a36bb3940e161c7085fe03b
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-05-09 19:20:25 +0100

    Adds single item checkout calculation

--- jasmine/spec/ShopSpec.js
+++ jasmine/spec/ShopSpec.js
@@ -1,6 +1,11 @@
 describe("Shop", function() {
-  it("checkout returns 50 for item A", function() {
+  it("returns 50 for checking out item A", function() {
     var shop = new Shop
     expect(shop.checkout("A")).toEqual(50)
+  })
+
+  it("returns 30 for checking out item B", function() {
+    var shop = new Shop
+    expect(shop.checkout("B")).toEqual(30)
   })
 })
--- jasmine/src/Shop.js
+++ jasmine/src/Shop.js
@@ -2,10 +2,24 @@ function Shop() {
 
 }
 
+// | Item | Price | Special offers |
+// +------+-------+----------------+
+// | A    | 50    | 3A for 130     |
+// | B    | 30    | 2B for 45      |
+// | C    | 20    |                |
+// | D    | 15    |         
+
+prices = {
+  A: 50,
+  B: 30,
+  C: 20,
+  D: 15
+}
+
 Shop.prototype = {
   constructor: Shop,
 
-  checkout: function() {
-    return 50
+  checkout: function(shoppingItem) {
+    return prices[shoppingItem]
   }
 }
